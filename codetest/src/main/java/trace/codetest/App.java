bf7cb35f9729ae93f42aba6359e34f11e667f7d4
Fernando Agostinho <agostinho.ufsc@gmail.com>
2016-12-21 12:03:23 -0200

    tracecode start

---
+++ codetest/src/main/java/trace/codetest/App.java
@@ -0,0 +1,20 @@
+package trace.codetest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	
+	public static void metodo1(String[] args) {
+		System.out.println("x1");
+		System.out.println("x2");
+		System.out.println("x3");
+	}
+	
+	public static void metodo2(String[] args) {
+		System.out.println("y1");
+		System.out.println("y2");
+	}
+	
+}
